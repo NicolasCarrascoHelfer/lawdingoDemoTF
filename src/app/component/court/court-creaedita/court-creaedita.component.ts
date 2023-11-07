df10b87b703e1d9b5c98a433d20b7d1099dc3b92
Nicolas Carrasco <u202116532@upc.edu.pe>
2023-11-07 01:17:33 -0500

    courts

--- src/app/component/court/court-creaedita/court-creaedita.component.ts
+++ src/app/component/court/court-creaedita/court-creaedita.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import { CourtService } from 'src/app/service/court.service';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Court } from 'src/app/model/court';
+import { District } from 'src/app/model/district';
+import { DistrictService } from 'src/app/service/district.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-court-creaedita',
   templateUrl: './court-creaedita.component.html',
   styleUrls: ['./court-creaedita.component.css']
 })
-export class CourtCreaeditaComponent {
+export class CourtCreaeditaComponent implements OnInit{
+  form: FormGroup = new FormGroup({});
+  court: Court = new Court();
+  mensaje: string = '';
+  id: number = 0;
+  edicion: boolean = false;
+  listaDistritos: District[] = [];
+  idDistritoSeleccionado:number=0
+  constructor(private cS:CourtService, private dS: DistrictService, private router:Router, private formBuilder: FormBuilder, private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id'];
+      this.edicion = data['id'] != null;
+      this.init();
+      
+    });
+    this.form = this.formBuilder.group({
+      idCourt: ['',],
+      name: ['', Validators.required],
+      district: ['', Validators.required],
+    });
+    this.dS.list().subscribe((data)=>{
+      this.listaDistritos=data;
+    })
+    
+  }
+  aceptar(): void{
+    if(this.form.valid){
+      this.court.idCourt=this.form.value.idCourt;
+      this.court.name=this.form.value.name;
+      this.court.district.idDistrict=this.form.value.district;
+      if (this.edicion) {
+        this.cS.update(this.court).subscribe(() => {
+          this.cS.list().subscribe((data) => {
+            this.cS.setList(data);
+          });
+        });
+      } else {
+        this.cS.insert(this.court).subscribe((data) => {
+          this.cS.list().subscribe((data) => {
+            this.cS.setList(data);
+          });
+        });
+      }
+      this.router.navigate(['courts'])
+    }else{
+      this.mensaje='Ingrese todos los campos'
+    }
+  }
+  obtenerControlCampo(nombreCampo: string): AbstractControl {
+    const control = this.form.get(nombreCampo);
+    if (!control) {
+      throw new Error(`Control no encontrado para el campo ${nombreCampo}`);
+    }
+    return control;
+  }
+  init() {
+    if (this.edicion) {
+      this.cS.listId(this.id).subscribe((data) => {
+        this.form = new FormGroup({
+          idCourt: new FormControl(data.idCourt),
+          name: new FormControl(data.name),
+          district: new FormControl(data.district)
+        });
+      });
+    }
+  }
 
 }
--- src/app/component/court/court-listar/court-listar.component.ts
+++ src/app/component/court/court-listar/court-listar.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Court } from 'src/app/model/court';
+import { CourtService } from 'src/app/service/court.service';
 
 @Component({
   selector: 'app-court-listar',
   templateUrl: './court-listar.component.html',
   styleUrls: ['./court-listar.component.css']
 })
-export class CourtListarComponent {
+export class CourtListarComponent implements OnInit{
+  dataSource: MatTableDataSource<Court> = new MatTableDataSource();
+  mensaje: string = '';
+  idVacio: boolean = false;
+  displayedColumns: string[] = [
+    'codigo',
+    'juzgado',
+    'distrito',
+    'accion01',
+    'accion02',
+    
+  ];
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private cS: CourtService, private formBuilder: FormBuilder) {
+  }
+  ngOnInit() {
+    this.cS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+    this.cS.getList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+  eliminar(id: number) {
+    this.cS.delete(id).subscribe((data) => {
+      this.cS.list().subscribe((data) => {
+        this.cS.setList(data);
+      });
+    });
+  }
+  filter(en: any) {
+    this.dataSource.filter = en.target.value.trim();
+  }
 }
