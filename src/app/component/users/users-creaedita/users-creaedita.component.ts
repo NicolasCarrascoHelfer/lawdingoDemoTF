29c3fae1ebfab50bd11387ae3d55124ec5d2b781
Camila Diaz <Yhadiraflores109@gmail.com>
2023-11-11 01:59:22 -0500

    Eliminar usuario

--- src/app/component/users/users-creaedita/users-creaedita.component.ts
+++ src/app/component/users/users-creaedita/users-creaedita.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { UsersService } from 'src/app/service/users.service';
 import { Users } from 'src/app/model/users';
+
 import {
   FormGroup,
   FormControl,
@@ -23,8 +24,25 @@ export class UsersCreaeditaComponent implements OnInit {
   maxFecha: Date = moment().add(-1, 'days').toDate();
 
   birthDay = new FormControl(new Date());
+  
   id: number = 0;
   edicion: boolean = false;
+
+
+
+  /*booleanOptions: BooleanOption[] = [
+    { value: true, viewValue: 'True' },
+    { value: false, viewValue: 'False' },
+  ];*/
+  booleanOptions:{value: boolean; viewValue: string}[] = [
+
+    { value: true, viewValue: 'True' },
+    { value: false, viewValue: 'False' },
+
+  ];
+
+
+  
 
   constructor(
     private uS: UsersService,
@@ -37,6 +55,7 @@ export class UsersCreaeditaComponent implements OnInit {
     this.route.params.subscribe((data: Params) => {
       this.id = data['id'];
       this.edicion = data['id'] != null;
+    
       this.init();
     });
     this.form = this.formBuilder.group({
@@ -47,13 +66,18 @@ export class UsersCreaeditaComponent implements OnInit {
       phone_num: ['', Validators.required],
       dni: ['', Validators.required],
       birthDay: ['', Validators.required],
+      lawyer: ['', Validators.required],
       username: ['', Validators.required],
+
       
 
     });
+
+
   }
   aceptar(): void {
     if (this.form.valid) {
+
       this.usuario.idUser = this.form.value.idUser;
       this.usuario.name = this.form.value.name;
       this.usuario.email = this.form.value.email;
@@ -62,13 +86,16 @@ export class UsersCreaeditaComponent implements OnInit {
       this.usuario.dni = this.form.value.dni;
       this.usuario.birthDay = this.form.value.birthDay;
       this.usuario.username = this.form.value.username;
+      this.usuario.lawyer= this.form.value.lawyer;
+
 
       if (this.edicion) {
         this.uS.update(this.usuario).subscribe(() => {
           this.uS.list().subscribe((data) => {
             this.uS.setList(data);
           });
-        });
+        },
+        );
       } else {
         this.uS.insert(this.usuario).subscribe((data) => {
           this.uS.list().subscribe((data) => {
@@ -90,17 +117,20 @@ export class UsersCreaeditaComponent implements OnInit {
     return control;
   }
   init() {
+
+
     if (this.edicion) {
       this.uS.listId(this.id).subscribe((data) => {
-        this.form = new FormGroup({
+        this.form = new  FormGroup({
           idUser: new FormControl(data.idUser),
           name: new FormControl(data.name),
           email: new FormControl(data.email),
           password:new FormControl(data.password),
+         lawyer:new FormControl(data.lawyer),
           phone_num:new FormControl(data.phone_num),
           dni:new FormControl(data.dni),
           birthDay: new FormControl(data.birthDay),
-          username:new FormControl(data.username),
+          username:new FormControl(data.username)
         });
       });
     }
--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { enviroment } from 'src/enviroments/enviroment';
-import { Observable, Subject } from 'rxjs';
+import {  Subject } from 'rxjs';
 import { Users } from '../model/users';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
@@ -27,7 +27,14 @@ export class UsersService {
     });
   }
   insert(p: Users) {
-    return this.http.post(`${this.url}/save`, p);
+
+    let token = sessionStorage.getItem('token');
+
+    return this.http.post(`${this.url}/save`, p,{
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
   }
   setList(listaNueva: Users[]) {
     this.listaCambio.next(listaNueva);
@@ -36,12 +43,27 @@ export class UsersService {
     return this.listaCambio.asObservable();
   }
   listId(id: number) {
-    return this.http.get<Users>(`${this.url}/${id}`);
+    let token = sessionStorage.getItem('token');
+    return this.http.get<Users>(`${this.url}/${id}`, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
   }
   update(c: Users) {
-    return this.http.put(this.url, c);
+    let token = sessionStorage.getItem('token');
+    return this.http.put(this.url, c, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
   }
   delete(id: number) {
-    return this.http.delete(`${this.url}/${id}`);
+    let token = sessionStorage.getItem('token');
+    return this.http.delete(`${this.url}/${id}`,{
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
   }
 }
